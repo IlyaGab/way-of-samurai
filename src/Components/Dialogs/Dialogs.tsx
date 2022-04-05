295bee9ab1f65a0d9a49e36228109064c5ea3902
IlyaGab <ilyagab1994@gmail.com>
2022-04-05 23:06:48 +0300

    add NavLinks to users

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import clases from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
         <div className={clases.dialogs}>
             <div className={clases.dialogs_items}>
-                <div className={clases.dialog}>Dimych</div>
-                <div className={clases.dialog}>Andrey</div>
-                <div className={clases.dialog}>Sasha</div>
-                <div className={clases.dialog}>Ilya</div>
-                <div className={clases.dialog}>Alena</div>
+                <div className={clases.dialog + ' ' + clases.active}>
+                    <NavLink to='/dialogs/1'>Dimych </NavLink></div>
+                <div className={clases.dialog}>
+                    <NavLink to='/dialogs/2'>Andrey</NavLink></div>
+                <div className={clases.dialog}>
+                    <NavLink to='/dialogs/3'>Sasha</NavLink></div>
+                <div className={clases.dialog}>
+                    <NavLink to='/dialogs/4'>Ilya</NavLink></div>
+                <div className={clases.dialog}>
+                    <NavLink to='/dialogs/5'> Alena</NavLink></div>
+            </div>
+            <div className={clases.messages}>
                 <div className={clases.message}>Hi</div>
                 <div className={clases.message}>How is your IT-KAMASUTRA?</div>
                 <div className={clases.message}>Yo</div>
