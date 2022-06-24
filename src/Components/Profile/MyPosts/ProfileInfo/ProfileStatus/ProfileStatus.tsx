e298c8144b4f0fb148f1d1fb827e009768c20b82
IlyaGab <ilyagab1994@gmail.com>
2022-06-24 16:08:47 +0300

    add task 71

---
+++ src/Components/Profile/MyPosts/ProfileInfo/ProfileStatus/ProfileStatus.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+type ProfileStatusType = {
+    status: string
+}
+
+
+class ProfileStatus extends React.Component <ProfileStatusType> {
+    state = {
+        editMode: false
+    }
+
+    activateEditMode  ()  {
+        this.setState({
+            editMode: true
+        })
+    }
+
+    deactivateEditMode  ()  {
+        this.setState({
+            editMode: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.editMode &&
+                <div>
+                    <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                </div>
+                }
+
+                {this.state.editMode &&
+                <div>
+                    <input onBlur={this.deactivateEditMode.bind(this)} autoFocus value={this.props.status}/>
+                </div>
+                }
+            </div>
+        );
+    };
+}
+
+export default ProfileStatus;
